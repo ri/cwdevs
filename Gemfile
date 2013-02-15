683b8e93c7247a3acd526f4dfe1f4bbc4f93cf2a
Ri Liu <ri.liu8@gmail.com>
2013-02-14 23:34:37 -0600

    added events section

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "http://rubygems.org"
 
 gem 'sinatra'
 gem 'sass'
+gem 'feedzirra'
 
 group :development do
   gem 'shotgun'
--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
 require 'sass'
+require 'json'
 
 APP_ROOT = File.expand_path(File.dirname(__FILE__))
-$LOAD_PATH << "#{APP_ROOT}/lib"
+
+events = JSON.parse(File.read("#{APP_ROOT}/public/data/events.json"))
+puts events["item"].first["title"]
 
 get '/' do
+  today = Time.now
+  events = events.reject { }
+  
   erb :"index.html"
 end
