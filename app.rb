491ca9024eab78ab43b1141c486fd6720ae0ba98
Ri Liu <ri.liu8@gmail.com>
2013-02-19 23:42:10 -0600

    Redirect to cwdevs.com domain

--- app.rb
+++ app.rb
@@ -4,7 +4,12 @@ require 'json'
 
 APP_ROOT = File.expand_path(File.dirname(__FILE__))
 
+before do
+  unless development?
+    redirect "http://www.cwdevs.com#{request.fullpath}" if request.host != "www.cwdevs.com"
+  end
+end
+
 get '/' do
-  
   erb :"index.html"
 end
