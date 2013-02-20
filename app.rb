3508c114bf4bee7d6e9c560c2758fc05108deec1
Ri Liu <ri.liu8@gmail.com>
2013-02-19 18:13:06 -0600

    updated layout

--- app.rb
+++ app.rb
@@ -4,12 +4,7 @@ require 'json'
 
 APP_ROOT = File.expand_path(File.dirname(__FILE__))
 
-events = JSON.parse(File.read("#{APP_ROOT}/public/data/events.json"))
-puts events["item"].first["title"]
-
 get '/' do
-  today = Time.now
-  events = events.reject { }
   
   erb :"index.html"
 end
